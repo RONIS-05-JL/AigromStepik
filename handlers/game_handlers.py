ecb8bb1edabcf2e0003eec3d5c87ebf76041114d
Ronis <ranislatypov@inbox.ru>
2024-06-03 00:47:56 +0300

    first BOT(inline buttons append)

--- handlers/game_handlers.py
+++ handlers/game_handlers.py
@@ -1,11 +1,9 @@
 from aiogram import Router, F
-from aiogram.types import Message
 from lexicon.text_patterns import LexRU, new_commands
 from games import Change_number, rock
 from Database.Data import databaser
 from aiogram.filters import Filter, Command
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, Message,
-                           ReplyKeyboardRemove, ReplyKeyboardMarkup)
+from aiogram.types import ( Message,ReplyKeyboardRemove,Message)
 from keybords.under_keybords import web_app_keyboard, mini_game_keybords
 
 router = Router()
@@ -18,20 +16,20 @@ async def mini_games(message: Message):
 
 @router.message(lambda message: message.text in ['rock'] + rock.list_items)
 async def rock_games(message: Message):
-    ans ,flag= rock.rock_game(message)
+    ans, flag = rock.rock_game(message)
     if not flag:
         await message.answer(text=ans, reply_markup=ReplyKeyboardRemove())
-
     else:
         await message.answer(text=ans, reply_markup=rock.rock_btn.as_markup(resize_keyboard=True))
 
 
 @router.message(Command(commands='games_stats'))
 async def games_stats(message: Message):
-    ans=databaser(message, returner=True)[1]['games']
-    for game,stats in ans.items():
-        await message.answer(f'Ваша статистика в игре  {game}\n'+
-                             '\n'.join(f'{key} : {stats[key]}' for key in ['all_games','wins','loses']))
+    ans = databaser(message, returner=True)[1]['games']
+    for game, stats in ans.items():
+        await message.answer(f'Ваша статистика в игре  {game}\n' +
+                             '\n'.join(f'{key} : {stats[key]}' for key in ['all_games', 'wins', 'loses']))
+
 
 # @router.message(lambda message: message.text == 'change_number' or
 #                                 databaser(message, returner=True)[1]['games']['change_number']['in_game'])
